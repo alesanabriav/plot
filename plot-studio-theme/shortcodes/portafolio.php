770e5900b7174400cabe4ffb28cbc3e56a852a05
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-01 20:06:41 -0500

    get categories

--- plot-studio-theme/shortcodes/portafolio.php
+++ plot-studio-theme/shortcodes/portafolio.php
@@ -10,9 +10,9 @@ function ps_portafolio_sc( $atts ) {
 	
 	$items = array_map(function($item) {
 		$attachment_id = get_post_thumbnail_id($item->ID);
-		$item->post_thumbnail = wp_get_attachment_image_src($attachment_id, 'thumbnail');
-		$item->post_image = wp_get_attachment_image_src($attachment_id, 'full');
-
+		$item->post_thumbnail = wp_get_attachment_image_src($attachment_id, 'thumbnail')[0];
+		$item->post_image = wp_get_attachment_image_src($attachment_id, 'full')[0];
+		$item->post_categories = get_the_category($item->ID);
 		return $item;
 	}, $query->get_posts());
 
